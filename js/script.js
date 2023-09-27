2eb1dc5556dec4a0a3187d069789a76ae58a8695
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-27 15:18:48 +0200

    [dd]:image with binding

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
+      text:'Hello Vue!!',
+      imageVuejs:'https://vuejs.org/images/logo.png'
       
     }
   }
-
-
-
+  
 }).mount('#app')
